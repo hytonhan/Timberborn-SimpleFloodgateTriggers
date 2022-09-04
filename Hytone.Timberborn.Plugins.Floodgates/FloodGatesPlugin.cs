06caf6a7a3e57675588057362bcdc1a9796eb1e0
Hytone <hannu_94@hotmail.com>
2022-09-04 10:53:05 +0300

    Moved localization labels from code to lang file

--- Hytone.Timberborn.Plugins.Floodgates/FloodGatesPlugin.cs
+++ Hytone.Timberborn.Plugins.Floodgates/FloodGatesPlugin.cs
@@ -22,37 +22,11 @@ namespace Hytone.Timberborn.Plugins.Floodgates
             var harmony = new Harmony("hytone.plugins.floodgatetriggers");
             harmony.PatchAll();
 
-            //AddLabels();
-
             TimberAPI.DependencyRegistry.AddConfigurator(new FloodGatesUIFragmentConfigurator(), SceneEntryPoint.InGame);
             TimberAPI.DependencyRegistry.AddConfigurator(new FloodgateEntityActionConfigurator(), SceneEntryPoint.InGame);
             TimberAPI.DependencyRegistry.AddConfigurator(new ScheduleSystemConfigurator(), SceneEntryPoint.InGame);
 
             Logger.LogInfo("FloodgateTriggersPlugin is loaded.");
         }
-
-        /// <summary>
-        /// Add Labels used by FloodGateUIFragment
-        /// </summary>
-        //private void AddLabels()
-        //{
-        //    TimberAPI.Localization.AddLabel("Floodgate.Triggers.EnableOnDroughtStarted", "Set height when drought starts");
-        //    TimberAPI.Localization.AddLabel("Floodgate.Triggers.EnableOnDroughtEnded", "Set height when drought ends");
-        //    TimberAPI.Localization.AddLabel("Floodgate.Schedule.Enable", "Set height on a schedule");
-        //    TimberAPI.Localization.AddLabel("Floodgate.Schedule.DisableOnDrought", "Disable schedule during drought");
-        //    TimberAPI.Localization.AddLabel("Floodgate.Schedule.DisableOnTemperate", "Disable schedule during temperate");
-        //    TimberAPI.Localization.AddLabel("Floodgate.Triggers.Basic", "Basic");
-        //    TimberAPI.Localization.AddLabel("Floodgate.Triggers.Advanced", "Advanced");
-        //    TimberAPI.Localization.AddLabel("Floodgate.Triggers.AttachToStreamGauge", "Attach Stream Gauge");
-        //    TimberAPI.Localization.AddLabel("Floodgate.Triggers.PickStreamGaugeTitle", "Pick a Stream Gauge");
-        //    TimberAPI.Localization.AddLabel("Floodgate.Triggers.PickStreamGaugeTip", "No really, pick any.");
-        //    TimberAPI.Localization.AddLabel("Floodgates.Triggers.NoLinks", "No Stream Gauges attached");
-        //    TimberAPI.Localization.AddLabel("Floodgates.Triggers.NoFloodgateLinks", "No Floodgates linked");
-        //    TimberAPI.Localization.AddLabel("Floodgates.Triggers.LinkedFloodgates", "Linked Floodgates");
-        //    TimberAPI.Localization.AddLabel("Floodgates.Triggers.Threshold1", "Stream Gauge low threshold");
-        //    TimberAPI.Localization.AddLabel("Floodgates.Triggers.Threshold2", "Stream Gauge high threshold");
-        //    TimberAPI.Localization.AddLabel("Floodgates.Triggers.HeightWhenBelowThreshold1", "Floodgate height below low threshold");
-        //    TimberAPI.Localization.AddLabel("Floodgates.Triggers.HeightWhenAboveThreshold2", "Floodgate height above high threshold");
-        //}
     }
 }
