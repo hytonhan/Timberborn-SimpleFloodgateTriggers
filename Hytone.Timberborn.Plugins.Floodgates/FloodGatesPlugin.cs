618f18489f5c9e2a1a074042ad76c0be0e718197
Hytone <hannu_94@hotmail.com>
2022-10-06 21:19:20 +0300

    Remvoed BepInEx dependency

--- Hytone.Timberborn.Plugins.Floodgates/FloodGatesPlugin.cs
+++ Hytone.Timberborn.Plugins.Floodgates/FloodGatesPlugin.cs
@@ -1,16 +1,12 @@
-﻿using BepInEx;
-using BepInEx.Logging;
-using HarmonyLib;
+﻿using HarmonyLib;
 using TimberApi.ConsoleSystem;
 using TimberApi.ModSystem;
 
 namespace Hytone.Timberborn.Plugins.Floodgates
 {
-    [BepInPlugin("hytone.plugins.floodgatetriggers", "FloodgateTriggersPlugin", "3.0.0")]
     [HarmonyPatch]
-    public class FloodGatesPlugin : BaseUnityPlugin, IModEntrypoint
+    public class FloodGatesPlugin : IModEntrypoint
     {
-        internal static new ManualLogSource Logger;
         public void Entry(IMod mod, IConsoleWriter consoleWriter)
         {
             var harmony = new Harmony("hytone.plugins.floodgatetriggers");
